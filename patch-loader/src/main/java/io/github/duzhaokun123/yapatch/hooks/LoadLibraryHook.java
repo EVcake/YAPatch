83842b0f2f306473f58d8cbf0d77314b9988610b
o0kam1 <duzhaokun2@outlook.com>
2024-10-29 13:32:52 +0800

    fix: host native lib load

--- patch-loader/src/main/java/io/github/duzhaokun123/yapatch/hooks/LoadLibraryHook.java
+++ patch-loader/src/main/java/io/github/duzhaokun123/yapatch/hooks/LoadLibraryHook.java
@@ -13,11 +13,22 @@ public class LoadLibraryHook {
         var pm = context.getPackageManager();
         Pine.hook(System.class.getDeclaredMethod("loadLibrary", String.class), new MethodHook() {
             @Override
-            public void afterCall(Pine.CallFrame callFrame) throws Throwable {
+            public void beforeCall(Pine.CallFrame callFrame) throws Throwable {
+                callFrame.setResult(null);
+                Log.d(TAG, "loadLibrary called" + callFrame.args[0], callFrame.getThrowable());
                 String libName = (String) callFrame.args[0];
-                if (!callFrame.hasThrowable()) return;
-                if (!(callFrame.getThrowable() instanceof UnsatisfiedLinkError)) return;
                 Log.d(TAG, "Loading library " + libName);
+
+                var thisSourceDir = pm.getApplicationInfo(context.getPackageName(), 0).sourceDir;
+                var thisSoPath = thisSourceDir + "!/lib/arm64-v8a/lib" + libName + ".so";
+                Log.d(TAG, "Trying to load from " + thisSoPath);
+                try {
+                    System.load(thisSoPath);
+                    Log.d(TAG, "Loaded library " + thisSoPath);
+                    return;
+                } catch (UnsatisfiedLinkError ignored) {
+                }
+
                 for (String module : modules) {
                     String sourceDir;
                     try {
@@ -31,11 +42,12 @@ public class LoadLibraryHook {
                     try {
                         System.load(soPatch);
                         Log.d(TAG, "Loaded library " + soPatch);
-                        callFrame.setThrowable(null);
                         return;
                     } catch (UnsatisfiedLinkError ignored) {
                     }
                 }
+                Log.d(TAG, "Failed to load library " + libName);
+                callFrame.setThrowable(new UnsatisfiedLinkError("Failed to load library " + libName));
             }
         });
     }
