76a572fa07a934b251ae56255f8fc01405fb16b6
o0kam1 <duzhaokun2@outlook.com>
2024-11-07 18:06:32 +0800

    release: 0.1.6

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.duzhaokun123.yapatch"
         minSdk = 33
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.1.5"
+        versionCode = 7
+        versionName = "0.1.6"
     }
 
     androidResources {
